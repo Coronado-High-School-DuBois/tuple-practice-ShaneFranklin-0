dfb7203aed72be5f83c7d4c178ff9cb0117d3ed1
Shane Franklin <shane.franklin@d11.org>
2024-10-31 18:17:21 +0000

    Done

--- tuple.py
+++ tuple.py
@@ -1,10 +1,13 @@
 print("Question 1:")
 #Q1: Create a tuple with single item 50 and print your tuple
+tuple0 = (50,)
+print(tuple0)
 
 print()
 print("Question 2:")
 #Q2: Access the value 4 and print it from tuple1
 tuple1 = ("Purple", "Pink", 17, [1, 2, 3, 4, 5], (5, 15, 25))
+print(tuple1[3][3])
 
 
 print()
@@ -14,5 +17,9 @@ print("Question 3:")
 # b. Print the length
 # c. Print the first item in your tuple
 # d. Print the last item in your tuple
-
+tuple2 = ("Spaghetti", "Fried Chicken", "Ramen Noodles", "Pizza", "Caesar Salad")
+print(tuple2)
+print(len(tuple2))
+print(tuple2[0])
+print(tuple2[-1])
 
